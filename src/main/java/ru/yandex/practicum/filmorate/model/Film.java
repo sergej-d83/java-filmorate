bdcb8f3c4209d01d2a374e967a391dc4bd1aeace
Sergej83 <sergej.d83@yandex.ru>
2022-08-09 22:48:58 +0300

    commit for review #1

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -2,7 +2,6 @@ package ru.yandex.practicum.filmorate.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NonNull;
 
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
@@ -13,13 +12,12 @@ import java.time.LocalDate;
 @AllArgsConstructor
 public class Film {
     private int id;
-    @NonNull
-    @NotBlank
+    @NotBlank(message = "Название не может быть пустым.")
     private String name;
-    @Size(max = 200)
+    @Size(max = 200, message = "Максимальная длина описания — 200 символов.")
     private String description;
     private LocalDate releaseDate;
-    @Min(value = 1)
+    @Min(value = 1, message = "Продолжительность фильма должна быть положительной.")
     private int duration;
 
 }
--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -13,13 +13,13 @@ import java.time.LocalDate;
 @AllArgsConstructor
 public class User {
     private int id;
-    @NotBlank
-    @Email
+    @NotBlank(message = "Электронная почта не может быть пустой.")
+    @Email(message = "Электронная почта должна быть в правильном формате.")
     private String email;
-    @NotBlank
-    @Pattern(regexp = "^\\S*$")
+    @NotBlank(message = "Логин не может быть пустым.")
+    @Pattern(regexp = "^\\S*$", message = "Логин не может содержать пробелы.")
     private String login;
     private String name;
-    @Past
+    @Past(message = "Дата рождения не может быть в будущем.")
     private LocalDate birthday;
 }
